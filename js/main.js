23a481fb3aa7a9cffcf77d5e8d997affe19ba673
Fenris-c <anatolijv236@gmail.com>
2019-11-11 20:05:49 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+
+    /**
+     * MAIN SLIDER
+     */
+    $("#mainSlider").owlCarousel(
+        {
+            dots: false,
+            loop: true,
+            margin: 10,
+            navText: ["", ""],
+            autoplay: true,
+            autoplayTimeout: 3000,
+            autoplayHoverPause: true,
+            responsiveClass: true,
+            responsive: {
+                600: {
+                    items: 1,
+                    nav: false,
+                },
+                1200: {
+                    items: 1,
+                    nav: true,
+                }
+            }
+        }
+    );
+});
